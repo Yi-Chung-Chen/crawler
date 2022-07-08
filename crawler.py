1de8bd557393c9ff3fc152cce4b27c984681c4c2
chung <94212671+Yi-Chung-Chen@users.noreply.github.com>
2022-07-08 20:53:57 +0800

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -7,7 +7,9 @@ movie_names = []
 movie_ratings = []
 movie_years = []
 movie_content = []
+movie_img_url = []
 num_movie = 40
+
 links = pd.read_csv('Mylinks.csv')
 
 def get_movies_info():
@@ -20,8 +22,7 @@ def get_movies_info():
                 
             request = req.Request(url , headers={
                 "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
-            }) #避免發生403 https request failed
-
+            })
             with req.urlopen(request) as response:
                 data = response.read().decode('utf-8')
 
@@ -34,26 +35,26 @@ def get_movies_info():
                 movie_names.append(names.string)
             else:
                 names = str(names.string)
-                movie_names.append(names[16:]) #扣除original title : 這個字串
+                movie_names.append(names[16:])
 
 
             ratings = root.find('span',attrs = {"class":"sc-7ab21ed2-1 jGRxWM"})
             years = root.find('span',attrs = {"class":"sc-8c396aa2-2 itZqyK"})
             content = root.find('span',attrs = {"class":"sc-16ede01-2 gXUyNh"})
-
+            img = root.find_all('a',attrs = {"class":"ipc-lockup-overlay ipc-focusable"})
+            movie_img_url.append("https://www.imdb.com/"+str(img[0].get('href'))) #設0 就可以只拿到電影封面就好
+            
             movie_ratings.append(ratings.string)
             movie_years.append(years.string)
             movie_content.append(content.string)
-
-def movie_writer(): # 將爬取的資訊寫入csv檔案
+            
+def movie_writer():
     file = open('movie_info.csv',mode='w', newline='')
     writer = csv.writer(file)
     for i in range(num_movie):
-        writer.writerow([movie_names[i],movie_ratings[i],movie_years[i],movie_content[i]])
+        writer.writerow([movie_names[i],movie_ratings[i],movie_years[i],movie_content[i],movie_img_url[i]])
 
 
 if __name__ == '__main__':
     get_movies_info()
     movie_writer()
-    #爬一次資料所需時間大約為 90.98256850242615 秒
-
